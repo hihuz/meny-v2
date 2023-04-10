f381992e9e629d1f6a70fe07feba423056ac4245
Sylvain Boulade <sboulade@gmail.com>
2023-04-10 15:17:39 +0200

    Setup swagger documentation on app startup

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,20 +1,41 @@
 import { RootModule } from './modules/RootModule';
 import { Logger } from '@nestjs/common';
-import { ApiConfig } from '@infrastructure/config/Api';
 
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ApiConfig } from '@infrastructure/config/Api';
+
+const DEFAULT_API_NAME = 'Meny API';
+const DEFAULT_DESCRIPTION = 'Documentation for the Meny API.';
 
 export class Server {
   private readonly port: number = ApiConfig.PORT;
+  private readonly title: string = ApiConfig.API_NAME || DEFAULT_API_NAME;
+  private readonly description: string =
+    ApiConfig.API_DESCRIPTION || DEFAULT_DESCRIPTION;
+  private readonly version: string = ApiConfig.API_VERSION;
 
   public async run(): Promise<void> {
     const app: NestExpressApplication =
       await NestFactory.create<NestExpressApplication>(RootModule);
 
+    this.buildSwaggerDocumentation(app);
     this.log();
 
     await app.listen(this.port);
+  }
+
+  private buildSwaggerDocumentation(app: NestExpressApplication) {
+    const options = new DocumentBuilder()
+      .setTitle(this.title)
+      .setDescription(this.description)
+      .setVersion(this.version)
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup('documentation', app, document);
   }
 
   private log(): void {
