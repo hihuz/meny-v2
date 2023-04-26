a359b42615bb62238040441fa295c3f56572b0b7
Sylvain Boulade <sboulade@gmail.com>
2023-04-26 11:36:21 +0200

    Add crud endpoints and handlers to recipe controller

--- src/application/api/controllers/RecipeController.ts
+++ src/application/api/controllers/RecipeController.ts
@@ -1,9 +1,17 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   HttpStatus,
   Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
   Query,
+  Req,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { RecipeUseCases } from '@core/domain/usecases/Recipe';
@@ -12,6 +20,12 @@ import { ApiResponse } from '@nestjs/swagger';
 import { RecipeDtoList } from '@core/domain/dto/recipe/RecipeDtoList';
 import { ListOptionsQueryDto } from '@core/common/dto/ListOptionsQueryDto';
 import { PipeConfig } from './config/Pipe';
+import { RecipeDto } from '@core/domain/dto/recipe/RecipeDto';
+import { UpsertRecipeDto } from '@core/domain/dto/recipe/UpsertRecipeDto';
+import { RequestWithUser } from '@application/types/auth';
+import { Roles } from '../decorators/Roles';
+import { Role } from '@core/common/enums/Role';
+import { RecipeOwnerGuard } from '../guards/RecipeOwnerGuard';
 
 @Controller('recipes')
 export class RecipeController {
@@ -27,5 +41,34 @@ export class RecipeController {
     query: ListOptionsQueryDto,
   ): Promise<RecipeDtoList> {
     return this.recipeUseCases.getList(query);
+  }
+
+  @Post()
+  @Roles(Role.Author)
+  @ApiResponse({ status: HttpStatus.CREATED, type: RecipeDto })
+  createRecipe(
+    @Body(new ValidationPipe(PipeConfig.bodyValidation)) body: UpsertRecipeDto,
+    @Req() request: RequestWithUser,
+  ) {
+    return this.recipeUseCases.create(body, request.user);
+  }
+
+  @Patch(':id')
+  @UseGuards(RecipeOwnerGuard)
+  @Roles(Role.Author)
+  @ApiResponse({ status: HttpStatus.OK, type: RecipeDto })
+  updateRecipe(
+    @Body(new ValidationPipe(PipeConfig.bodyValidation)) body: UpsertRecipeDto,
+    @Param('id', new ParseIntPipe()) id: number,
+  ) {
+    return this.recipeUseCases.update(body, id);
+  }
+
+  @Delete(':id')
+  @UseGuards(RecipeOwnerGuard)
+  @Roles(Role.Author)
+  @ApiResponse({ status: HttpStatus.OK, type: RecipeDto })
+  deleteRecipe(@Param('id', new ParseIntPipe()) id: number) {
+    return this.recipeUseCases.delete(id);
   }
 }
--- src/application/modules/RecipeModule.ts
+++ src/application/modules/RecipeModule.ts
@@ -4,11 +4,14 @@ import { Module } from '@nestjs/common';
 import { RecipeRepository } from '@infrastructure/adapter/prisma/repository/Recipe';
 import { RecipeTokens } from '@core/domain/di/tokens/Recipe';
 import { PrismaClient } from '@infrastructure/adapter/prisma/client/PrismaClient';
+import { UserTokens } from '@core/domain/di/tokens/User';
+import { UserRepository } from '@infrastructure/adapter/prisma/repository/User';
 
 @Module({
   controllers: [RecipeController],
   providers: [
     PrismaClient,
+    { provide: UserTokens.UserPort, useClass: UserRepository },
     { provide: RecipeTokens.RecipePort, useClass: RecipeRepository },
     { provide: RecipeTokens.RecipeUseCase, useClass: RecipeService },
   ],
